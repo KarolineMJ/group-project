72110fadd8fb979e0fbf4fdf26dd79caa47c4025
KarolineMJ <Karoline_MJ@hotmail.com>
2017-10-31 13:34:12 +0100

    halleluja

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let clickCounter = 0;
+
+let starter = document.querySelector("#starter");
+let typeScene = document.querySelector("#typer");
+let startButton = document.querySelector("#startButton");
+let typedText = document.querySelector("#typedtext");
+let typeSound = document.querySelector("#typing");
+let rainSound = document.querySelector("#rain");
+let movieDiv = document.querySelector("#movie");
+let movieOne = document.querySelector("#movieOne");
+
+
+startButton.addEventListener('click', startAnimation);
+
+function startAnimation(){
+	clickCounter = clickCounter + 1;
+
+	if(clickCounter ==1){
+		console.log("this is working");
+		typeScene.style.display ="block";
+		starter.style.display ="none";
+		typewriter();
+		typeSound.play();
+		rainSound.play();
+	}
+}
+
+
+
 // set up text to print, each item in array is new line
 var aText = new Array(
 "Los Angeles, 1945",
@@ -16,8 +45,7 @@ var iTextPos = 0; // initialise text position
 var sContents = ''; // initialise contents variable
 var iRow; // initialise current row
 
-function typewriter()
-{
+function typewriter(){
  sContents =  ' ';
  iRow = Math.max(0, iIndex-iScrollAt);
  var destination = document.getElementById("typedtext");
@@ -33,9 +61,23 @@ function typewriter()
    iArrLength = aText[iIndex].length;
    setTimeout("typewriter()", 500);
   }
+  else{
+  	typedText.classList.add('enable');
+  	typeSound.pause();
+  	rainSound.pause();
+  }
  } else {
   setTimeout("typewriter()", iSpeed);
  }
+};
+
+typedText.addEventListener('animationend', startMovie);
+
+function startMovie(){
+	console.log("hey you");
+	typeScene.style.display = "none";
+	movieDiv.style.display = "block";
+	movieOne.play();
 }
 
-typewriter();
+
